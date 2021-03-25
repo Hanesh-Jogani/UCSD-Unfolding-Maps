bc8677435fb853034cae1b6f3f2d0ed7137d0f17
HANESH JOGANI <62371149+Hanesh-Jogani@users.noreply.github.com>
2021-03-25 10:22:02 +0530

    Add files via upload

---
+++ UCSD Unfolding Maps/src/demos/Student.java
@@ -0,0 +1,25 @@
+package demos;
+
+/** A class to store information about a Student
+ * 
+ * @author Hanesh
+ * 
+ */
+public class Student extends Person
+{
+	public Student(String name)  {
+		super(name);
+	}
+
+	public boolean isAsleep( int hr ) // override 
+	{ 
+		return 2 < hr && 8 > hr; 
+	}
+	
+	public static void main(String[] args)
+	{
+		Person p;
+		p = new Student("Sally");
+		p.status(1);
+	}
+}
